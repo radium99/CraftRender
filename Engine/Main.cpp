6cc895d77ff9bc1893b81726d990861057676ee0
radium99 <115422121+radium99@users.noreply.github.com>
2026-02-25 15:24:05 +0900

    창 크기 변경

--- Engine/Main.cpp
+++ Engine/Main.cpp
@@ -2,116 +2,127 @@
 #include <string>
 #include <stdint.h>
 
-// 콜백 함수.
-// 창 메시지 처리할 때 사용.
 LRESULT CALLBACK WindowProc(
-    HWND hwnd,
-    UINT uMsg,
-    WPARAM wParam,
-    LPARAM lParam
+	HWND hwnd,
+	UINT uMsg,
+	WPARAM wParam,
+	LPARAM lParam
 );
 
 int main()
 {
-    // 그래픽스 프로그래밍.
-    // 창(Window)을 생성하고, API를 활용해서 창에 그림을 그리는 프로그램.
-    // Win32 API 활용.
 
-    // 운영체제에 창 등록할 때 사용할 클래스 이름 - 유일해야함.
-    std::wstring className = L"Craft Engine Window";
-    std::wstring title = L"Craft Engine";
 
-    // 창 크기.
-    uint32_t width = 1280;
-    uint32_t height = 800;
 
-    // 창 생성에 필요한 정보(구조체) 채우기.
-    HINSTANCE hInstance = GetModuleHandle(nullptr);
-    WNDCLASS wc = { };
-    wc.lpfnWndProc = WindowProc;
-    wc.hInstance = hInstance;       // 프로그램 포인터.
-    wc.lpszClassName = className.c_str();
+	std::wstring className = L"Craft Engine Window";
+	std::wstring title = L"Craft Engine";
 
-    // 클래스 등록.
-    if (!RegisterClass(&wc))
-    {
-        return 0;
-    }
+	uint32_t width = 1280;
+	uint32_t height = 800;
 
-    // 창 객체 생성.
-    HWND hwnd = CreateWindow(
-        className.c_str(),               // Window class
-        title.c_str(),                   // Window text
-        WS_OVERLAPPEDWINDOW,            // Window style
 
-        // 위치 및 크기
-        CW_USEDEFAULT, CW_USEDEFAULT,
-        width, height,
+	HINSTANCE hInstance = GetModuleHandle(nullptr);
+	WNDCLASS wc = { };
+	wc.lpfnWndProc = WindowProc;
+	wc.hInstance = hInstance;
+	wc.lpszClassName = className.c_str();
 
-        nullptr,       // Parent window    
-        nullptr,       // Menu
-        hInstance,      // Instance handle
-        nullptr        // Additional application data
-    );
 
-    // 창 생성 실패 처리.
-    if (!hwnd)
-    {
-        return 0;
-    }
+	if (!RegisterClass(&wc))
+	{
+		return 0;
+	}
 
-    // 창 보이기 설정.
-    ShowWindow(hwnd, SW_SHOW);
 
-    // 창에서 발생하는 메시지 처리 루프.
-    // GetMessage - 동기 방식(Blocking 방식).
-    // PeekMessage - 비동기 방식(Non Blocking 방식).
-    // 동기: 처리되기 전까지 다른 동작 안함.
-    // 비동기: 처리되지 않아도 넘어감.
-    MSG msg = { };
-    // 창 종료 메시지가 발생할 때까지 실행.
-    while (msg.message != WM_QUIT)
-    {
-        // 창 메시지 처리.
-        // 비동기 방식으로 메시지가 들어왔는지 확인.
-        if (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE))
-        {
-            // 전달받은 메시지를 적절하게 변환.
-            TranslateMessage(&msg);
+	RECT rect = {};
+	rect.left = 0;
+	rect.top = 0;
+	rect.right = width;
+	rect.bottom = height;
+	AdjustWindowRect(&rect, WS_OVERLAPPEDWINDOW, FALSE);
 
-            // 변환한 메시지를 처리 함수에 전달.
-            DispatchMessage(&msg);
-        }
 
-        // 창 메시지가 없으면 엔진 로직 처리.
-        else
-        {
+	uint32_t windowWidth = rect.right - rect.left;
+	uint32_t windowHeight = rect.bottom - rect.top;
 
-        }
-    }
 
-    return 0;
+	uint32_t positionX = (GetSystemMetrics(SM_CXSCREEN) - windowWidth) / 2;
+	uint32_t positionY = (GetSystemMetrics(SM_CYSCREEN) - windowHeight) / 2;
+
+
+	HWND hwnd = CreateWindow(
+		className.c_str(),               // Window class
+		title.c_str(),                   // Window text
+		WS_OVERLAPPEDWINDOW,            // Window style
+
+
+		positionX, positionY,
+		windowWidth, windowHeight,
+
+		nullptr,       // Parent window    
+		nullptr,       // Menu
+		hInstance,      // Instance handle
+		nullptr        // Additional application data
+	);
+
+
+	if (!hwnd)
+	{
+		return 0;
+	}
+
+
+	//RECT rect2;
+	//GetClientRect(hwnd, &rect2);
+
+
+	ShowWindow(hwnd, SW_SHOW);
+
+
+	MSG msg = { };
+
+	while (msg.message != WM_QUIT)
+	{
+
+
+		if (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE))
+		{
+
+			TranslateMessage(&msg);
+
+
+			DispatchMessage(&msg);
+		}
+
+	
+		else
+		{
+
+		}
+	}
+
+	return 0;
 }
 
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
-    switch (uMsg)
-    {
-    case WM_DESTROY:
-        PostQuitMessage(0);
-        return 0;
+	switch (uMsg)
+	{
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
 
-    case WM_PAINT:
-    {
-        PAINTSTRUCT ps;
-        HDC hdc = BeginPaint(hwnd, &ps);
+	case WM_PAINT:
+	{
+		PAINTSTRUCT ps;
+		HDC hdc = BeginPaint(hwnd, &ps);
 
-        // All painting occurs here, between BeginPaint and EndPaint.
-        FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW + 1));
-        EndPaint(hwnd, &ps);
-    }
-    return 0;
+		// All painting occurs here, between BeginPaint and EndPaint.
+		FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW + 1));
+		EndPaint(hwnd, &ps);
+	}
+	return 0;
 
-    }
-    return DefWindowProc(hwnd, uMsg, wParam, lParam);
+	}
+	return DefWindowProc(hwnd, uMsg, wParam, lParam);
 }
